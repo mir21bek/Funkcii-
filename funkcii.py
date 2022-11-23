3c7980ed695ce81eb900f75bdf5d6c674965d369
mir21bek <mirbek.2121@gmail.com>
2022-11-23 18:01:28 +0600

    работа с функциями

---
+++ funkcii.py
@@ -0,0 +1,55 @@
+# # ФУНКЦИИ вложенные и авторские
+# from random import randint
+
+# s in fio_split:
+#         first_letter_from_fio += fs[0]
+#     return rand_num+first_letter_from_fio+age
+
+# def get_login():
+#     pass
+
+# if name == 'main':
+#     print(get_pasdef sign_in():
+#     fio = input('FIO: ')
+#     age = input('Age: ')
+#     return fio, age
+
+# def get_password():
+#     fio, age = sign_in()
+#     rand_num = str(randint(10000, 99999))
+#     fio_split = fio.split()
+#     first_letter_from_fio = ''
+#     for fsword())
+
+
+
+
+# list_1 = ['name', 'age', '1', '19']
+
+# def half_reversd(list_data):
+#     list_1.reverse()
+#     a = list_data[:len(list_data) // 2]
+#     b = list_data[len(list_data) // 2:]
+#     print(b + a)
+# half_reversd(list_1)
+
+
+'''
+2. Создайте с помощью рекурсии функцию которая генерирует 10 чисел Фибоначчи:
+
+1,1,2,3,5,8,13,21,34,...
+'''
+
+# def cal_sum(a):
+#     if a == []:
+#         return 0
+#     else:
+#         summ = cal_sum(a[1:])
+#         # print(summ)
+#         summ = summ + a[0]
+#         print(summ)
+#         return summ
+# l = [1,1,2,3,5,8,13,21,34]
+# summ = cal_sum(l)
+# print('summ =', summ)
+
